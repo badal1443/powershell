a7bf0d330c13b076aea4ea4a026aafe4992d0ca7
chhunka java ee eclipse <badal1443@gmail.com>
2016-12-14 22:58:30 -0800

    commiting first 2 powershell scripts.

---
+++ Intro1.ps1
@@ -0,0 +1,14 @@
+﻿#Commenting a line with hash symbol
+
+<#
+Comment block
+#>
+
+#region
+echo "this is a region , collapsable block to keep code more nice looking and easy to read and debug"
+echo "Hello Powershell!"
+#endregion
+
+#region Titled region, this is optional though
+
+#endregion end title to keep block separate
---
+++ Intro2.ps1
@@ -0,0 +1,34 @@
+﻿#---------------------------------------------------------------------------
+#Cmdlets
+#---------------------------------------------------------------------------
+
+#region cmdlets
+
+#Get-Command : Get all the commands available in powershell for this computer
+Get-Command
+
+
+#Classify/search for just a verb or noun
+
+Get-Command -verb "get"
+Get-Command -verb "set"
+Get-Command -noun "service"
+Get-Command -noun "ChildItem"
+Get-Command -noun "WmiObject"
+
+#endregion cmdlets
+
+#region code section 2
+
+Get-Help Get-Command -examples
+Get-Help Get-Command -full
+Get-Help Get-Command -Detailed
+Get-Help Get-Command -Online
+#endregion code section 2
+
+#region code block 3
+
+# -? option can be used to get quick help, it' a short cut to use as desbribed below.
+
+Get-Command -?
+#endregion code block 3
